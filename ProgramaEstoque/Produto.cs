53d5dfbb165af90c1552211b89f9d690182f62ca
carlos <geraldocarlos503@gmail.com>
2026-02-06 13:25:28 -0300

    teste

--- ProgramaEstoque/Produto.cs
+++ ProgramaEstoque/Produto.cs
@@ -1,6 +1,52 @@
-﻿namespace ProgramaEstoque;
+using System.Threading;
+
+namespace ProgramaEstoque;
 
 public class Produto
 {
+    public string Nome;
+    public double Preco;
+    public int Quantidade;
 
+
+    public double ValorTotalEmEstoque()
+    {
+        return Preco * Quantidade;
+    }
+
+    public void AdicionarQuantidadeProduto()
+    {
+        Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+        int quantidadeProduto = int.Parse(Console.ReadLine());
+        Quantidade += quantidadeProduto;
+        AnimarCarregamento();
+        Console.Clear();
+        Console.WriteLine($"Dados atualizados: {Nome}, ${Preco}, " +
+                         $" {Quantidade} unidades, Total: " + ValorTotalEmEstoque().ToString("F2"));
+
+    }
+
+    private static void AnimarCarregamento()
+    {
+        Console.Write("Processando... ");
+        string animacao = "|/-\\";
+        for (int i = 0; i < 20; i++)
+        {
+            Console.Write(animacao[i % 4]);
+            Thread.Sleep(100);
+            Console.Write("\b");
+        }
+        Console.WriteLine();
+    }
+
+    public void RemoverQuantidadeProduto()
+    {
+        Console.Write("Digite o número de produtos a ser removido do estoque: ");
+        int quantidade = int.Parse(Console.ReadLine());
+        Quantidade -= quantidade;
+        AnimarCarregamento();
+        Console.Clear();
+        Console.WriteLine($"Dados atualizados: {Nome}, ${Preco}, " +
+                          $"{Quantidade} unidades, Total: " + ValorTotalEmEstoque().ToString("F2"));
+    }
 }
--- ProgramaEstoque/Program.cs
+++ ProgramaEstoque/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ProgramaEstoque
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {   Produto prod = new();
+
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome: ");
+            prod.Nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            prod.Preco = double.Parse(Console.ReadLine());
+
+            Console.Write("Quantidade em estoque: ");
+            prod.Quantidade = int.Parse(Console.ReadLine());
+
+            double total = prod.ValorTotalEmEstoque();
+            Console.WriteLine($"Dados do prdouto: {prod.Nome}, ${prod.Preco}, {prod.Quantidade} unidades, Total: {total.ToString("F2")} ");
+     
+            prod.AdicionarQuantidadeProduto();
+
+            prod.RemoverQuantidadeProduto();
+
+        }
+    }
+}
