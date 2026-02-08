9a89a994d74489032a310ba46a8b7a63ba24f981
carlos <geraldocarlos503@gmail.com>
2026-02-08 12:02:30 -0300

    Exemplo programa para cadastrar funcionario

---
+++ ProgramaFuncionarios/Funcionarios.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace ProgramaFuncionarios;
+
+public class Funcionarios
+{
+    public string Nome;
+    public double SalarioBruto, Imposto;
+
+    public double SalarioLiquido()
+    {
+        return SalarioBruto - Imposto;
+    }
+
+    public double AlmentarSalario(double porcentagem)
+    {
+        SalarioBruto += SalarioBruto * (porcentagem / 100.00);
+        return SalarioLiquido();
+
+    }
+
+    public override string ToString()
+    {
+        return Nome + ", "
+              + "$ " 
+              + SalarioLiquido().ToString("F2", CultureInfo.InstalledUICulture);
+    }
+}
---
+++ ProgramaFuncionarios/Program.cs
@@ -0,0 +1,21 @@
+﻿using ProgramaFuncionarios;
+
+
+Funcionarios f = new();
+
+Console.Write("Nome: ");
+f.Nome = Console.ReadLine();
+
+Console.Write("Salario Bruto: ");
+f.SalarioBruto = double.Parse(Console.ReadLine());
+
+Console.Write("Imposto: ");
+f.Imposto = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Funcionarios: " + f);
+
+Console.Write("Digite a porcentagem para aumentar o salário: ");
+double por = double.Parse(Console.ReadLine());
+f.AlmentarSalario(por);
+
+Console.WriteLine("Dados atualizados: " + f);
