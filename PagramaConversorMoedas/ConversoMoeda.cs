d556f7e8f7989ecb51c4900db26c319b83cf42c0
carlos <geraldocarlos503@gmail.com>
2026-02-08 21:14:17 -0300

    Coversor de moedas

---
+++ PagramaConversorMoedas/ConversoMoeda.cs
@@ -0,0 +1,13 @@
+﻿namespace PagramaConversorMoedas;
+
+public class ConversoMoeda
+{
+    public static double Iof = 6.0 / 100;
+
+    public static double ValorDaCompra(double cotacao, double quantia)
+    {
+        double total = quantia * cotacao;
+        return total + (total * Iof);
+    }
+
+}
---
+++ PagramaConversorMoedas/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.Contracts;
+using PagramaConversorMoedas;
+
+
+
+Console.Write("Qual é a cotação do dólar: ");
+double cotacao = double.Parse(Console.ReadLine());
+
+Console.Write("Quantos dólares você vai comprar: ");
+double quantia = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Valor a se pago em reias: " + ConversoMoeda.ValorDaCompra(cotacao, quantia).ToString("F2"));
+
+
